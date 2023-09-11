3ab9bca8876a5f96a7f2d207c0bec487cb394b78
Prathamesh <ov01809@gmail.com>
2023-09-11 16:55:46 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/HomePage.dart';
+// import 'package:flutterbasic/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int number=30;
+    String name="Prathamesh";
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Catalogue App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $number challenge by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
